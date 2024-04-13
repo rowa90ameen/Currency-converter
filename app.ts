7fe7cbac7452470f9517f400e6ab1abf1a2a0385
rowa90ameen <159520497+rowa90ameen@users.noreply.github.com>
2024-04-13 06:29:37 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const currency: any = {
+  USD: 1,
+  EUR: 0.91,
+  GBP: 0.76,
+  INR: 74.57,
+  PKR: 280,
+};
+let user_answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from Currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "to",
+    message: "Enter to Currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "amount",
+    message: "Enter your amount",
+    type: "number",
+  },
+]);
+
+let from_amount = currency[user_answer.from];
+let to_amount = currency[user_answer.to];
+let amount = user_answer.amount;
+let base_amount = amount / from_amount;
+let converted_amount = base_amount * to_amount;
+let roundedconverted_amount= converted_amount.toFixed(2)
+console.log(roundedconverted_amount);
